ff4ce12f9767fd661223c100786dcfd968622495
argeeee <argetimjusufi01@gmail.com>
2023-11-14 16:33:01 +0100

    feat: ✨ added service locator

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      ciao
+    </div>
+  )
+}
+
+export default App
---
+++ src/app/ServiceLocator.ts
@@ -0,0 +1,32 @@
+export class ServiceLocator {
+  private singletons: { [key: string]: any } = {};
+  private lazySingletons: { [key: string]: any } = {};
+  private types: { [key: string]: any } = {};
+
+  registerSingleton<T>(key: string, instance: T): void {
+    this.singletons[key] = instance;
+  }
+
+  registerLazySingleton<T>(key: string, getInstance: () => T): void {
+    this.lazySingletons[key] = getInstance;
+  }
+
+  registerType<T>(key: string, getInstance: () => T): void {
+    this.types[key] = getInstance;
+  }
+
+  resolve<T>(key: string): T {
+    if (this.singletons[key]) {
+			return this.singletons[key];
+    }
+		else if (this.lazySingletons[key]) {
+			this.singletons[key] = this.lazySingletons[key]();
+			return this.singletons[key];
+		}
+		else if (this.types[key]) {
+			return this.types[key]();
+		}
+
+		throw new Error(`Service with key ${key} not registered.`);
+  }
+}
